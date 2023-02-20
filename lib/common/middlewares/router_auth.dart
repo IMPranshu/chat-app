d27a1c728e22ddf7197c14c127c9da51d8425198
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-02-20 17:46:37 +0530

    Routing for Signin page added

--- lib/common/middlewares/router_auth.dart
+++ lib/common/middlewares/router_auth.dart
@@ -4,9 +4,11 @@ import 'package:chap/common/store/store.dart';
 
 import 'package:get/get.dart';
 
-/// 检查是否登录
+// check if the user has logged in
+// this route class gets called before user access any of the controller
+// we are using the middleware service provided by Getx
 class RouteAuthMiddleware extends GetMiddleware {
-  // priority 数字小优先级高
+  // priority variable the smaller the better
   @override
   int? priority = 0;
 
@@ -14,12 +16,14 @@ class RouteAuthMiddleware extends GetMiddleware {
 
   @override
   RouteSettings? redirect(String? route) {
-    if (UserStore.to.isLogin || route == AppRoutes.SIGN_IN || route == AppRoutes.INITIAL) {
+    if (UserStore.to.isLogin ||
+        route == AppRoutes.SIGN_IN ||
+        route == AppRoutes.INITIAL) {
       return null;
     } else {
-      Future.delayed(
-          Duration(seconds: 2), () => Get.snackbar("Tips", "Login expired, please login again!"));
-      return RouteSettings(name: AppRoutes.SIGN_IN);
+      Future.delayed(Duration(seconds: 2),
+          () => Get.snackbar("Tips", "Login expired, please login again!"));
+      return const RouteSettings(name: AppRoutes.SIGN_IN);
     }
   }
 }
