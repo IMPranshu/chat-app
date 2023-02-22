ef469627060633ee5a8466e7977085c992e53dad
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-02-23 00:07:16 +0530

    comments added for different put methods

--- lib/global.dart
+++ lib/global.dart
@@ -11,6 +11,10 @@ import 'package:get/get.dart';
 class Global {
   static Future init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    // in StorageService we have used inIt method which returns "Future" and is async
+    // if we want to load these type of controller we have to use putAsync to load it
+    // becuase it returns Future and it may delay for uncertain amount of time and to make sure that nothing crashes, we use putAsync
+    // also in database operation then it is saved in  GetxService methods and it returns future. In this case we use Get.putAsync to load and insert in the memory and so that we wait until everything is saved/done
     await Get.putAsync<StorageService>(() => StorageService().init());
     Get.put<UserStore>(UserStore());
   }
