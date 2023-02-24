218d964b1bc5c016acb284dbce7953c092bb06ee
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-02-25 00:18:34 +0530

    sign up widget added

--- lib/pages/frame/sign_in/view.dart
+++ lib/pages/frame/sign_in/view.dart
@@ -30,42 +30,49 @@ class SignInPage extends GetView<SignInController> {
   }
 
   Widget _buildThirdPartyLogin(String loginType, String logo) {
-    return Container(
-        width: 295.w,
-        height: 44.h,
-        padding: EdgeInsets.all(10.h),
-        margin: EdgeInsets.only(bottom: 15.h),
-        decoration: BoxDecoration(
-            color: AppColors.primaryBackground,
-            borderRadius: const BorderRadius.all(Radius.circular(5)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                // all the below three spreadradius, blurradius and offset is giving the button the floating effect that is generally used in modern designs
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: const Offset(0, 1),
-              ),
-            ]),
-        child: Row(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 40.2, right: 30.w),
-              child: Image.asset("assets/icons/$logo.png"),
-            ),
-            Container(
-              child: Text(
-                "Sign in with $loginType",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: AppColors.primaryText,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14.sp,
+    return GestureDetector(
+      child: Container(
+          width: 295.w,
+          height: 44.h,
+          padding: EdgeInsets.all(10.h),
+          margin: EdgeInsets.only(bottom: 15.h),
+          decoration: BoxDecoration(
+              color: AppColors.primaryBackground,
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  // all the below three spreadradius, blurradius and offset is giving the button the floating effect that is generally used in modern designs
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1),
                 ),
-              ),
-            )
-          ],
-        ));
+              ]),
+          child: Row(
+            mainAxisAlignment:
+                logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
+            children: [
+              logo == ''
+                  ? Container()
+                  : Container(
+                      padding: EdgeInsets.only(left: 40.2, right: 30.w),
+                      child: Image.asset("assets/icons/$logo.png"),
+                    ),
+              Container(
+                child: Text(
+                  "Sign in with $loginType",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.sp,
+                  ),
+                ),
+              )
+            ],
+          )),
+      onTap: () {},
+    );
   }
 
   Widget _buildOrWidget() {
@@ -93,6 +100,36 @@ class SignInPage extends GetView<SignInController> {
         ));
   }
 
+  Widget _buildSignUpWidget() {
+    return GestureDetector(
+      child: Column(
+        children: [
+          Text(
+            "Don't have an account?",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontWeight: FontWeight.normal,
+              fontSize: 12.sp,
+            ),
+          ),
+          GestureDetector(
+            child: Text(
+              "Sign up here.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.primaryElement,
+                fontWeight: FontWeight.normal,
+                fontSize: 12.sp,
+              ),
+            ),
+          ),
+        ],
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,7 +141,12 @@ class SignInPage extends GetView<SignInController> {
             _buildThirdPartyLogin("Google", "google"),
             _buildThirdPartyLogin("Facebook", "facebook"),
             _buildThirdPartyLogin("Apple", "apple"),
-            _buildOrWidget()
+            _buildOrWidget(),
+            _buildThirdPartyLogin("phone number", ""),
+            SizedBox(
+              height: 35.h,
+            ),
+            _buildSignUpWidget()
           ],
         ),
       ),
