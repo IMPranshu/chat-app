10c6efa922df83d4ac8048a3aefc6e18d2ea9b31
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-02-24 23:29:46 +0530

    logo widget added

--- lib/pages/frame/sign_in/view.dart
+++ lib/pages/frame/sign_in/view.dart
@@ -6,16 +6,37 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignInPage extends GetView<SignInController> {
   // as we have used GetView<> now we can access the variables from WelcomeController here
   const SignInPage({super.key});
 
+  Widget _buildLogo() {
+    // widget is something that we see on the screen
+    // a container is a widget so we can return a Container here it is totally valid
+    return Container(
+      margin: EdgeInsets.only(top: 100.h, bottom: 80.h),
+      child: Text(
+        "# Chap .",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontWeight: FontWeight.bold,
+          fontSize: 34.sp,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: AppColors.primarySecondaryBackground,
       body: Center(
-        child: Text("Sign-In Page"),
+        child: Column(
+          children: [_buildLogo()],
+        ),
       ),
     );
   }
